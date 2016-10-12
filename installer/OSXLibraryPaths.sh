b45fb8bafaf57931fab874808f3798fc3a250d55
Jason Champion <jchampion@zetacentauri.com>
2016-10-12 14:20:12 -0700

    Packaging and signing changes for osx.

--- installer/OSXLibraryPaths.sh
+++ installer/OSXLibraryPaths.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore Vorbital.app/Contents/MacOS/Vorbital
+# You may need to change this path if you're on another system or different version of Qt.
+cp ../../build-Vorbital-Desktop_Qt_5_7_0_clang_64bit-Release/Vorbital.app/Contents/MacOS/Vorbital Vorbital.app/Contents/MacOS/Vorbital
+install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets Vorbital.app/Contents/MacOS/Vorbital
+install_name_tool -change @rpath/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui Vorbital.app/Contents/MacOS/Vorbital
+install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore Vorbital.app/Contents/MacOS/Vorbital
+install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore Vorbital.app/Contents/Frameworks/QtGui.framework/Versions/5/QtGui
 install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/../Frameworks/libvorbisfile.3.dylib Vorbital.app/Contents/MacOS/Vorbital
