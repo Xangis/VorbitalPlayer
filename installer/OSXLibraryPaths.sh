88734a8159a3be164596a53e12fcd1488057880d
Jason Champion <jchampion@zetacentauri.com>
2016-10-14 17:51:51 -0700

    Package Qt frameworks with macdeployqt command.

--- installer/OSXLibraryPaths.sh
+++ installer/OSXLibraryPaths.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # You may need to change this path if you're on another system or different version of Qt.
 cp ../../build-Vorbital-Desktop_Qt_5_7_0_clang_64bit-Release/Vorbital.app/Contents/MacOS/Vorbital Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore Vorbital.app/Contents/MacOS/Vorbital
-install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore Vorbital.app/Contents/Frameworks/QtGui.framework/Versions/5/QtGui
 install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/../Frameworks/libsndfile.1.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/../Frameworks/libvorbisfile.3.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbis.0.dylib @executable_path/../Frameworks/libvorbis.0.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/opt/mpg123/lib/libmpg123.0.dylib @executable_path/../Frameworks/libmpg123.0.dylib Vorbital.app/Contents/MacOS/Vorbital
 install_name_tool -change /usr/local/opt/wavpack/lib/libwavpack.1.dylib @executable_path/../Frameworks/libwavpack.1.dylib Vorbital.app/Contents/MacOS/Vorbital
+# Framework sub-dependencies.
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib Vorbital.app/Contents/Frameworks/libvorbis.0.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/../Frameworks/libogg.0.dylib Vorbital.app/Contents/Frameworks/libvorbisfile.3.dylib
+install_name_tool -change /usr/local/Cellar/libvorbis/1.3.5/lib/libvorbis.0.dylib @executable_path/../Frameworks/libvorbis.0.dylib Vorbital.app/Contents/Frameworks/libvorbisfile.3.dylib
+# Qt
+/Users/Apple/Qt/5.7/clang_64/bin/macdeployqt Vorbital.app -appstore-compliant
 otool -L Vorbital.app/Contents/MacOS/Vorbital
