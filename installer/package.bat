dccce12a4f1d6607eb7c8a13b9b7ba3784e84d05
Jason Champion <jchampion@zetacentauri.com>
2016-10-24 15:47:05 -0700

    Update package command to sign app using non-auto-generated certificate.

--- installer/package.bat
+++ installer/package.bat
@@ -1,7 +1,5 @@
 echo This program runs the desktop app packager command to create a Windows 10 store package from an existing installer.
 pause
-DesktopAppConverter.exe -Installer "C:\Users\Xangis\code\VorbitalPlayer\installer\VorbitalPlayer4.31Setup.exe" -PackageName "VorbitalPlayer" -Version 4.3.1.0 -Makeappx -Destination "C:\Users\Xangis\code\VorbitalPlayer\installer" -InstallerArguments "/VERYSILENT" -Publisher "CN=Jason Champion" -Sign
-Certutil -addStore TrustedPeople VorbitalPlayer\auto-generated.cer
-
-
-
+DesktopAppConverter.exe -Installer "C:\Users\Xangis\code\VorbitalPlayer\installer\VorbitalPlayer4.31Setup.exe" -PackageName "VorbitalPlayer" -Version 4.3.1.0 -Makeappx -Destination "C:\Users\Xangis\code\VorbitalPlayer\installer" -InstallerArguments "/VERYSILENT" -Publisher "CN=Jason Champion"
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe" sign -f jc.pfx -fd SHA256 -v C:\Users\Xangis\code\VorbitalPlayer\installer\VorbitalPlayer\VorbitalPlayer.appx
+Certutil -addStore root jc.cer
